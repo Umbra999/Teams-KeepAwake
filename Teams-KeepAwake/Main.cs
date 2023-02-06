8dcb074386d3e81f6bc783491154dbc01b16c5d6
Umbra <69671761+Umbra999@users.noreply.github.com>
2023-02-06 10:17:05 +0100

    smoother movement and back fix

--- Teams-KeepAwake/Main.cs
+++ Teams-KeepAwake/Main.cs
@@ -5,23 +5,32 @@ namespace Teams_KeepAwake
     internal class Main
     {
         [DllImport("user32.dll")]
-        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
-        public static void MoveMouseBy(int xDelta, int yDelta)
-        {
-            mouse_event(0x0001, xDelta, yDelta, 0, 0);
-        }
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
 
         public static void Start()
         {
             Console.Title = "Teams KeepAwake by Umbra";
             Console.WriteLine("This Program prevents MS Teams from going into AFK mode by Moving the Mouse 1 pixel every 2 Seconds");
 
+
             while (true)
             {
-                MoveMouseBy(1, 1);
-                Console.WriteLine("Mouse ping sent");
+                MoveMouseBy(1, 0);
+                Thread.Sleep(100);
+                MoveMouseBy(1, 0);
+                Thread.Sleep(100);
+                MoveMouseBy(-1, 0);
+                Thread.Sleep(100);
+                MoveMouseBy(-1, 0);
+
                 Thread.Sleep(120000);
+
             }
+        }
+
+        public static void MoveMouseBy(int xDelta, int yDelta)
+        {
+            mouse_event(0x0001, xDelta, yDelta, 0, 0);
         }
     }
 }
